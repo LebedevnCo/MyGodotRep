62cfe1a95dfe05e16e61d9fac5678ee4d88c2a8b
Max Lebedev <lebedevmv@gmail.com>
2026-02-11 20:16:54 +0100

    Host connecction test2 V0.63

--- startmenu.gd
+++ startmenu.gd
@@ -31,7 +31,7 @@ func _ready():
 	#laodIP for multiplayer
 	show_local_ip()
 	multiplayer.connected_to_server.connect(_on_connected)
-	multiplayer.peer_connected.connect(_on_peer_connected)
+	#multiplayer.peer_connected.connect(_on_peer_connected)
 	
 	#laodscreen function
 	splash = $TextureRect
@@ -61,9 +61,21 @@ func _ready():
 # ---------- BUTTONS ----------
 
 func _on_start_b_pressed() -> void:
-	print("Start button pressed")
-	print("Loading scene:", selected_scene_path)
-	get_tree().change_scene_to_file(selected_scene_path)
+	# Multiplayer mode
+	if multiplayer.has_multiplayer_peer():
+		
+		# Only host can start
+		if multiplayer.is_server():
+			start_game.rpc(selected_scene_path, Global.difficulty)
+	
+	# Single player mode
+	else:
+		start_game(selected_scene_path, Global.difficulty)
+
+@rpc("authority", "call_local")
+func start_game(scene_path, difficulty):
+	Global.difficulty = difficulty
+	get_tree().change_scene_to_file(scene_path)
 
 func _on_exit_b_pressed() -> void:
 	get_tree().quit()
@@ -101,31 +113,31 @@ func _on_host_b_pressed():
 	var result = peer.create_server(7777)
 	
 	if result != OK:
-		print("Failed to host")
 		return
 		
 	multiplayer.multiplayer_peer = peer
-	print("Hosting started")
+	$VBoxContainer2/yIPLabel.text = "Hosting... My ID: " + \
+		str(multiplayer.get_unique_id())
+	
 
 func _on_join_b_pressed():
 	var ip = $VBoxContainer2/LineEdit.text.strip_edges()
 	
 	if ip == "":
-		print("No IP entered")
 		return
 		
 	var peer = ENetMultiplayerPeer.new()
 	var result = peer.create_client(ip, 7777)
 	
 	if result != OK:
-		print("Failed to connect")
 		return
 	
 	multiplayer.multiplayer_peer = peer
-	print("Connecting to ", ip)
+	
 
 func _on_connected():
-	$VBoxContainer2/yIPLabel.text = "Connected to server!"
+	$VBoxContainer2/yIPLabel.text = "Connected! My ID: " + \
+		str(multiplayer.get_unique_id())
 
-func _on_peer_connected(id):
-	$VBoxContainer2/yIPLabel.text = "Peer connected: " + str(id)
+#func _on_peer_connected(id):
+	#$VBoxContainer2/yIPLabel.text = "Peer connected: " + str(id)
